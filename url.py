d24f9644f7fecdc713a3988d04f98f2b7b517265
vdemjanovich <zaptot@gmail.com>
2017-09-02 18:56:37 +0300

    get new color

--- url.py
+++ url.py
@@ -1,25 +1,46 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
+#====================================================================================================================================================================================================================
+
+def getColor(driver, last):
+    color=driver.find_element_by_xpath("/html/body/div[3]/div[2]/div/div/div[1]/div/div[4]/div[3]/ul/li[1]")
+    number = driver.find_element_by_xpath("/html/body/div[3]/div[2]/div/div/div[1]/div/div[4]/div[4]/div[1]/span/span")
+    color=color.get_attribute('class')
+    number = int(number.text)
+    if number==last[0]+1:
+        i = 0
+        while not color[i] == ' ':
+            i+=1
+        color=color[i+1:]
+        round=color
+        last[0]=number
+        return round
+    else:
+        return 0
+def getNumberOfFirstGame(driver):
+    number=driver.find_element_by_xpath("/html/body/div[3]/div[2]/div/div/div[1]/div/div[4]/div[4]/div[1]/span/span")
+    #print(int(number.text))
+    return int(number.text)
+
+#====================================================================================================================================================================================================================
 
 driver=webdriver.PhantomJS()
 driver.set_window_size(1920,1080)
 driver.get('http://csgofast.com/#game/double')
-def getColor(driver):
-    color=driver.find_element_by_xpath("/html/body/div[3]/div[2]/div/div/div[1]/div/div[4]/div[3]/ul/li[1]")
-    number = driver.find_element_by_xpath("/html/body/div[3]/div[2]/div/div/div[1]/div/div[4]/div[4]/div[1]/span/span")
-    print(color.get_attribute('class'))
-    print(color.text)
-    print(int(number.text))
-    return 0
+last=[getNumberOfFirstGame(driver)]
 
-def getNumberOfFirstGame(driver):
-    number=driver.find_element_by_xpath("/html/body/div[3]/div[2]/div/div/div[1]/div/div[4]/div[4]/div[1]/span/span")
-    print(int(number.text))
-    return 0
+i=0
 
-#first=int(getNumberOfFirstGame(driver))
-getColor(driver)
-
+while i<100:
+    round = getColor(driver, last)
+    if round:
+        print(round)
+        #print("true")
+    else:
+        time.sleep(20)
+        #print("false")
+    i+=1
 
 
 driver.quit()
